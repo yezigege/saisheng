a6dae4df732b31aa67074ceee4ec643aacc3a177
yezigege <1757423278@qq.com>
2020-07-04 15:52:10 +0800

    生成数据库并创建超级用户

--- tops/models.py
+++ tops/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+
+
+# 分数表
+class Score(models.Model):
+    client = models.CharField(verbose_name='客户端号', max_length=16, unique=True)
+    score = models.IntegerField(verbose_name='分数', default=0,
+                                validators=[MaxValueValidator(10000000), MinValueValidator(1)])
+
+    def __str__(self):
+        return f'{self.client}'
+
+    class Meta:
+        verbose_name = '分数登记表'
+        verbose_name_plural = verbose_name
+
+
+# 名次表
+class Rank(models.Model):
+    c_id = models.OneToOneField(Score, on_delete=models.CASCADE, primary_key=True, )
+    rank = models.IntegerField(verbose_name='名次', validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return f'{self.c_id}'
+
+    class Meta:
+        verbose_name = '名次表'
+        verbose_name_plural = verbose_name
